505d84a64dd9156d8fc267e5bffde861c4349e7f
Vadzim <49336945+comtvset@users.noreply.github.com>
2024-11-28 00:48:06 +0100

    feat: adaptive design for mobile devices
    * feat: adaptive design for mobile devices
    
    * fix: burger hover stickness, correct select for safari and firefox
    
    * fix: add arrow down for select lenguges

--- src/components/form/getStarted/GetStarted.tsx
+++ src/components/form/getStarted/GetStarted.tsx
@@ -1,12 +1,21 @@
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 
-export const GetStarted = () => {
+interface GetStartedProps {
+  closeMenu: () => void;
+  isMenuOpen: boolean;
+}
+
+export const GetStarted: React.FC<GetStartedProps> = ({ closeMenu, isMenuOpen }) => {
   const t = useTranslations('HomePage');
 
   return (
     <>
-      <Link href={'signup'} className="globalBlueButton">
+      <Link
+        href={'signup'}
+        className={`globalBlueButton ${isMenuOpen ? 'globalBlueButtonBurger' : ''}`}
+        onClick={closeMenu}
+      >
         {t('getStart')}
       </Link>
     </>
