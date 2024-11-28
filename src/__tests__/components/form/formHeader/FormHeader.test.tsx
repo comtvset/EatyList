a42321e36f03a221d1741b4b400a3b3b5c1c99f2
comtvset <comtvset@gmail.com>
2024-11-28 01:55:11 +0100

    fix: FormHeader.test.tsx

--- src/__tests__/components/form/formHeader/FormHeader.test.tsx
+++ src/__tests__/components/form/formHeader/FormHeader.test.tsx
@@ -1,10 +1,16 @@
 import { FormHeader } from '@/components/form/formHeader/FormHeader';
 import { expect, it, describe } from '@jest/globals';
 import { render, screen } from '@testing-library/react';
+import { NextIntlClientProvider } from 'next-intl';
+import messages from '@/i18n/messages/en.json';
 
 describe('FormHeader Component', () => {
   it('should render form header', () => {
-    render(<FormHeader header="Sign In / Sign Up" />);
+    render(
+      <NextIntlClientProvider locale="en" messages={messages}>
+        <FormHeader header="Sign In / Sign Up" />
+      </NextIntlClientProvider>,
+    );
 
     const heading = screen.getByRole('heading', { level: 2 });
     expect(heading).toHaveTextContent('Sign In');
