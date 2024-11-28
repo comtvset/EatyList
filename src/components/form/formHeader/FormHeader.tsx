4bc37f7843d3f5b5ed904f760e6acd71e490b6e1
comtvset <comtvset@gmail.com>
2024-11-28 01:49:59 +0100

    feat: add question to Form Header, fix visible alert

--- src/components/form/formHeader/FormHeader.tsx
+++ src/components/form/formHeader/FormHeader.tsx
@@ -1,9 +1,35 @@
 import styles from '@/components/form/formHeader/FormHeader.module.scss';
+import { useTranslations } from 'next-intl';
+import Link from 'next/link';
 
 export const FormHeader = ({ header }: { header: string }) => {
+  const t = useTranslations('Form');
+
+  let question = '';
+  let link = '';
+  let href = '';
+
+  if (header === t('titleFormSignUp')) {
+    question = t('haveAccount?');
+    link = t('titleFormSignIn');
+    href = 'signin';
+  } else {
+    question = t('doNotHaveAccount?');
+    link = t('titleFormSignUp');
+    href = 'signup';
+  }
+
   return (
     <>
-      <h2 className={styles.header}>{header}</h2>
+      <div className={styles.formHeaderContainer}>
+        <h2 className={styles.header}>{header}</h2>
+        <div>
+          {question}{' '}
+          <Link className={styles.link} href={href}>
+            {link}
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
