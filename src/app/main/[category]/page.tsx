7fe578be6352ec08d2bff3aa5e290832a0abc6de
Vadzim <49336945+comtvset@users.noreply.github.com>
2024-12-02 19:15:16 +0100

    fix: correct alert add loader for main

--- src/app/main/[category]/page.tsx
+++ src/app/main/[category]/page.tsx
@@ -1,13 +1,19 @@
 import BackButton from '@/components/buttons/BackButton';
 import styles from '../../page.module.css';
+import { Suspense } from 'react';
+import Loading from '@/app/loading';
 
 export default function Page({ params }: { params: { category: string } }) {
   const { category } = params;
 
   return (
-    <div className={styles.main}>
-      Category: {category}
-      <BackButton />
-    </div>
+    <>
+      <Suspense fallback={<Loading />}>
+        <div className={styles.main}>
+          Category: {category}
+          <BackButton />
+        </div>
+      </Suspense>
+    </>
   );
 }
