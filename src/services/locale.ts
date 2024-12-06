ed00682f6bbcbe37fc23afd814b353b1d430c338
comtvset <comtvset@gmail.com>
2024-12-06 12:22:56 +0100

    fix: logging 12

--- src/services/locale.ts
+++ src/services/locale.ts
@@ -9,15 +9,6 @@ export async function getUserLocale() {
   return (await cookies()).get(COOKIE_NAME)?.value || defaultLocale;
 }
 
-// export async function setUserLocale(locale: Locale) {
-//   (await cookies()).set(COOKIE_NAME, locale);
-// }
-
 export async function setUserLocale(locale: Locale) {
-  (await cookies()).set(COOKIE_NAME, locale, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    maxAge: 60 * 60 * 24,
-    path: '/',
-  });
+  (await cookies()).set(COOKIE_NAME, locale);
 }
