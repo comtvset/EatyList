43a5b297299ca7d8b2525f66beb32c6a0eeb6871
comtvset <comtvset@gmail.com>
2024-12-06 02:27:13 +0100

    fix: logging 10

--- src/services/locale.ts
+++ src/services/locale.ts
@@ -9,6 +9,15 @@ export async function getUserLocale() {
   return (await cookies()).get(COOKIE_NAME)?.value || defaultLocale;
 }
 
+// export async function setUserLocale(locale: Locale) {
+//   (await cookies()).set(COOKIE_NAME, locale);
+// }
+
 export async function setUserLocale(locale: Locale) {
-  (await cookies()).set(COOKIE_NAME, locale);
+  (await cookies()).set(COOKIE_NAME, locale, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    maxAge: 60 * 60 * 24,
+    path: '/',
+  });
 }
